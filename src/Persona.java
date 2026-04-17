41ad2ea21136f7df51d934e928ffdf9e691ea402
Sabrina Vallejos Antunez <vallejosantunezs@gmail.com>
2026-04-17 09:33:22 -0300

    inicio de la clase Impresora

---
+++ src/Persona.java
@@ -0,0 +1,13 @@
+public class Persona {
+    private String nombre;
+    private long dni;
+    private String correo;
+
+    public Persona() {
+
+    }
+
+    public void SolicitarImpresion(){
+        
+    }
+}
