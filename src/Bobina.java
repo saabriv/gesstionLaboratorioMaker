a308ffa58a5258643bd4206d8760df68faaf96c6
Sabrina Vallejos Antunez <vallejosantunezs@gmail.com>
2026-04-22 09:27:57 -0300

    inicio de la clase Bobina

---
+++ src/Bobina.java
@@ -0,0 +1,44 @@
+public class Bobina {
+    String material;
+    String color;
+    Double gramos;
+    int id;
+
+    public Bobina(){}
+
+
+
+
+// getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Double getGramos() {
+        return gramos;
+    }
+
+    public void setGramos(Double gramos) {
+        this.gramos = gramos;
+    }
+}
